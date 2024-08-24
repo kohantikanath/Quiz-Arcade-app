827824c9a55af4dc8de05af81cfde5635a0bda53
Kohantika Nath <kohan.nath@gmail.com>
2024-08-24 11:20:15 +0530

    homepage done

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./HomePage.css";
+
+import homepagebg from "../../assets/HomePage.png"
+import Categories from '../../components/Categories';
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+        <div className='homepage-l'>
+            <h1>Quizes</h1>
+            <Categories />
+        </div>
+        <div className='homepage-r'>
+            <img src={homepagebg} alt=""  />
+        </div>
+
+    </div>
+  )
+}
+
+export default HomePage
